29995170ce39e87bfa7de7c1f3cb0f13d3f9ac2e
herval <hervalfreire@gmail.com>
2014-07-20 16:19:56 +0200

    allow configuring a refresh interval + handle disconnections from twitter

--- lib/twitter_client.rb
+++ lib/twitter_client.rb
@@ -4,6 +4,7 @@ class TwitterClient
 
   def initialize(configuration_options)
     @search_term = configuration_options[:search_term]
+    @refresh_seconds = configuration_options[:refresh_seconds].to_i
     @client = Twitter::REST::Client.new do |config|
       config.consumer_key = configuration_options[:consumer_key]
       config.consumer_secret = configuration_options[:consumer_secret]
@@ -13,13 +14,18 @@ class TwitterClient
   def listen
     latest_id = 0
     while true
-      @client.search(@search_term, :result_type => "recent").take(1).collect do |tweet|
-        if latest_id < tweet.id
-          latest_id = tweet.id
-          yield Tweet.new(tweet)
+      begin
+        @client.search(@search_term, :result_type => "recent").take(1).collect do |tweet|
+          if latest_id < tweet.id
+            latest_id = tweet.id
+            yield Tweet.new(tweet)
+          end
         end
+        sleep(@refresh_seconds)
+      rescue => e
+        puts "OH NOES! Can't connect to Twitter: #{e.message}"
+        sleep(10)
       end
-      sleep(5)
     end
   end
 
